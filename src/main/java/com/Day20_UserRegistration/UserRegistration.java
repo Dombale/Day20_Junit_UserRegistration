bd2ce7dc98b2b9f14ef9893745e14c00b43ea82a
Priyanka <priyankadombale27@gmail.com>
2022-03-20 13:10:57 +0530

    Added:  Valid Phone Number regex

--- src/main/java/com/Day20_UserRegistration/UserRegistration.java
+++ src/main/java/com/Day20_UserRegistration/UserRegistration.java
@@ -1,24 +1,77 @@
 package com.Day20_UserRegistration;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	static String regex;
 
-	private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-
-	public boolean validateFirstName(String fname) {
-		Pattern pattern = Pattern.compile(NAME_PATTERN);
-		return pattern.matches(NAME_PATTERN, fname);
+	/*
+	 * @purpose: As a User need to enter a valid First Name. First name starts with
+	 * Cap and has minimum 3 characters
+	 * 
+	 * @param: firstName
+	 * 
+	 * @function: To check first name is Valid or not
+	 * 
+	 * @returns true or false
+	 */
+	public boolean firstName(String firstName) {
+		regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
 	}
 
-	public boolean validateLastName(String lName) {
-		Pattern pattern = Pattern.compile(NAME_PATTERN);
-		return pattern.matches(NAME_PATTERN, lName);
+	/*
+	 * @purpose: As a User need to enter a valid last Name. Last name starts with
+	 * Cap and has minimum 3 characters
+	 * 
+	 * @param: lastName
+	 * 
+	 * @function: To check last name is Valid or not
+	 * 
+	 * @returns true or false
+	 */
+	public boolean lastName(String lastName) {
+		regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
 	}
 
-	public boolean validateEmail(String email) {
-		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-		return pattern.matches(EMAIL_PATTERN, email);
+	/*
+	 * @purpose: As a User need to enter a valid email E.g. abc.xyz@bl.co.in - Email
+	 * has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @
+	 * and . positions
+	 * 
+	 * @param: email
+	 * 
+	 * @function: To check email is Valid or not
+	 * 
+	 * @returns true or false
+	 */
+	public boolean email(String email) {
+		regex = "^[a-z]+[+-_.]*[a-z0-9]*[@][a-z]+[.][a-z]{2,4}[.]*([a-z]{2})*$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+	/*
+	 * @purpose: As a User need to follow pre defined Mobile Format - E.g. 91
+	 * 9919819801 - Country code follow by space and 10 digit number
+	 * 
+	 * @param: mobileNumber
+	 * 
+	 * @function: To check mobile Number is Valid or not
+	 * 
+	 * @returns true or false
+	 */
+	public boolean mobileNumber(String mobileNumber) {
+		regex = "^[91]+[ ]?[6-9]{1}[0-9]{9}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(mobileNumber);
+		return matcher.matches();
 	}
 }
--- src/test/java/com/Day20_UserRegistration/UserRegistrationTest.java
+++ src/test/java/com/Day20_UserRegistration/UserRegistrationTest.java
@@ -1,24 +1,56 @@
 package com.Day20_UserRegistration;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+	UserRegistration userRegistration = new UserRegistration();
 
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserRegistration userRegistration = new UserRegistration();
-		userRegistration.validateFirstName("Priyanka");
+	public void givenFirstName_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Priyanka");
+		Assert.assertEquals(true, result);
 	}
 
 	@Test
-	public void givenLastName_WhenProper_ShouldReturnTrue() {
-		UserRegistration userRegistration = new UserRegistration();
-		userRegistration.validateLastName("Dombale");
+	public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.firstName("priyanka");
+		Assert.assertEquals(false, result);
 	}
 
 	@Test
-	public void givenEmail_WhenProper_ShouldReturnTrue() {
-		UserRegistration userRegistration = new UserRegistration();
-		userRegistration.validateEmail("priyankadombale27@gmail.com");
+	public void givenLastName_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Dombale");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.lastName("yamagar");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenEmail_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.email("abc.xyz@bl.co.in");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.email("abc.xyz.in");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenMobileNumber_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.mobileNumber("91 9919819801");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenMobileNumber_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.mobileNumber("9919819801");
+		Assert.assertEquals(false, result);
 	}
 }
