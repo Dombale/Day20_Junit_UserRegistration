7f5b4df481370ceb8dd8f7772ab1500c483d084f
Priyanka <priyankadombale27@gmail.com>
2022-03-20 13:44:51 +0530

    Added: must have at least one special character in password

--- src/test/java/com/Day20_UserRegistration/UserRegistrationTest.java
+++ src/test/java/com/Day20_UserRegistration/UserRegistrationTest.java
@@ -89,4 +89,16 @@ public class UserRegistrationTest {
 		boolean result = userRegistration.password3("priyanka");
 		Assert.assertEquals(false, result);
 	}
+
+	@Test
+	public void givenPasswordRule4_WhenValid_ShouldReturnTrue() {
+		boolean result = userRegistration.password4("Priya@27");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPasswordRule4_WhenInvalid_ShouldReturnFalse() {
+		boolean result = userRegistration.password3("priya27");
+		Assert.assertEquals(false, result);
+	}
 }
